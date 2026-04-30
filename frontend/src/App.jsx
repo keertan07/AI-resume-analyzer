7676e7083cc60cffb3baec3ecf4ed253c57191b3
Keertan <Keertancr7@gmail.com>
2026-04-30 13:07:38 +0530

    updating API URL for production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     formData.append('resume', file)
     formData.append('jobDesc',jobDesc)
     try {
-      const response = await fetch('http://localhost:5000/analyze', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/analyze`, {
         method: 'POST',
         body: formData
       })
